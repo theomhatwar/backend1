3f10e053a9037b539b2a47617b2c1e65a67b0776
Om Vijay Hatwar <omhatwar2004@gmail.com>
2026-05-30 23:59:04 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app=express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// 2. Parse incoming JSON payloads (Form/API data)
+app.use(express.json({ limit: "16kb" }));
+
+// 3. Parse incoming URL-encoded data (URL parameters/queries)
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// 4. Serve static assets (images, PDFs, favicons) from the "public" folder
+app.use(express.static("public"));
+
+// 5. Parse browser cookies securely into req.cookies
+app.use(cookieParser());
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path:'./.env'
 })
 
 connectDB()
+.then(() => {
+    
+    app.on("error", (error) => {
+        console.log("Express app error:", error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+})
+
