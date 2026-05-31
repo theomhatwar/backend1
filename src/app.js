48ffc63cce2e4e70d9bf19972612f8153ad2eb4a
Om Vijay Hatwar <omhatwar2004@gmail.com>
2026-06-01 00:11:19 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -21,4 +21,13 @@ app.use(express.static("public"));
 // 5. Parse browser cookies securely into req.cookies
 app.use(cookieParser());
 
+// routes import
+
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+
 export {app}
